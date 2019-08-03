b574645790caefd3a17a66b408bb0566ac4c0778
Rafael Espinosa <xxx@xxx.com>
2019-08-03 19:50:39 +0200

    fixed pixel scaling

--- Assets/Scripts/Managers/CutsceneManager/ICutscene.cs
+++ Assets/Scripts/Managers/CutsceneManager/ICutscene.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class ICutscene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    CutsceneManager cutscene;
+
+    [SerializeField]
+    PlayerController player;
+
+    [SerializeField]
+    GameObject TitleScreen;
+
+    [SerializeField]
+    GameObject GameOverScreen;
+
+
+    void StartTitleScreen()
+    {
+
+    }
+
+    void ExitTitleScreen ()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
